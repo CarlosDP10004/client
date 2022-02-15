0566579f88a5395e65475bcf211f75270544d020
Martin Valle <martin_afii@hotmail.com>
2022-02-14 19:37:44 -0600

    Sidebar se agrego estilo e icono, se trabajo en el home y otros estilos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  today: Date = new Date();
+  pipe = new DatePipe('en-US');
+  todayWithPipe = null;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todayWithPipe = this.pipe.transform(Date.now(), 'dd/MM/yyyy');
   }
 
 }

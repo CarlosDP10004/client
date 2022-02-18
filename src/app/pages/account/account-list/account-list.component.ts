4aca382e781371249546dbdf08feee83fced0e74
cdiaztester <carlos.diaz.dp10004@gmail.com>
2022-02-17 20:56:42 -0600

    Se sube cambios en manto de cuentas

--- src/app/pages/account/account-list/account-list.component.ts
+++ src/app/pages/account/account-list/account-list.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/core/http/account.service';
+import Swal from 'sweetalert2';
+import { AccountEditComponent } from '../account-edit/account-edit.component';
 
 @Component({
   selector: 'app-account-list',
   templateUrl: './account-list.component.html',
   styleUrls: ['./account-list.component.scss']
 })
-export class AccountListComponent implements OnInit {
+export class AccountListComponent {
+  page: number = 1;
+  accounts: any[] = [];
+  bsModalRef: BsModalRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private accountService: AccountService,
+    private bsModalService: BsModalService,
+    private toastr: ToastrService
+  ) { 
+    this.showAll();
   }
+
+  showAll(){
+    this.accountService.showAll().subscribe(data => {
+      Object.assign(this.accounts, data);
+    }, error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: error,
+        confirmButtonColor: '#c9a892',
+        confirmButtonText: 'Aceptar'
+      })
+    });
+  }
+
+  editAccount(IdCuenta:number){
+    this.accountService.changeAccountId(IdCuenta);
+    this.bsModalRef = this.bsModalService.show(AccountEditComponent);
+    this.bsModalRef.content.event.subscribe(result => {
+      if (result == 'OK') {
+        setTimeout(() => {
+          this.showAll();
+        }, 5000);
+      }
+    });
+  }
+
+  changeStatus(){}
 
 }

281f06b1cb589f2a04f223d8618f15f343db0e67
cdiaztester <carlos.diaz.dp10004@gmail.com>
2022-02-22 12:53:57 -0600

    Se sube cambio del catalogo de proveedores

--- src/app/pages/provider/provider-list/provider-list.component.ts
+++ src/app/pages/provider/provider-list/provider-list.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ProviderService } from 'src/app/core/http/provider.service';
+import Swal from 'sweetalert2';
+import { ProviderAddComponent } from '../provider-add/provider-add.component';
+import { ProviderEditComponent } from '../provider-edit/provider-edit.component';
 
 @Component({
   selector: 'app-provider-list',
   templateUrl: './provider-list.component.html',
   styleUrls: ['./provider-list.component.scss']
 })
-export class ProviderListComponent implements OnInit {
+export class ProviderListComponent {
 
-  constructor() { }
+  page: number = 1;
+  providers: any[] = [];
+  bsModalRef: BsModalRef;
 
-  ngOnInit(): void {
+  constructor(
+    private bsModalService: BsModalService,
+    private providerService: ProviderService
+  ) {
+    this.showAll();
+   }
+  showAll() {
+    this.providerService.showAll().subscribe(data => {
+      Object.assign(this.providers, data);
+    }, error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: error,
+        confirmButtonColor: '#c9a892',
+        confirmButtonText: 'Aceptar'
+      })
+    });
+  }
+
+  addProvider(){
+    this.bsModalRef = this.bsModalService.show(ProviderAddComponent);
+    this.bsModalRef.content.event.subscribe(result => {
+      if (result == 'OK') {
+        this.showAll();
+      }
+    }); 
+  }
+
+  editProvider(IdProvider:number){
+    this.providerService.changeProviderId(IdProvider);
+    this.bsModalRef = this.bsModalService.show(ProviderEditComponent);
+    this.bsModalRef.content.event.subscribe(result => {
+      if (result == 'OK') {
+        this.showAll();
+      }
+    });
+  }
+
+  changeStatus(){
+
   }
 
 }

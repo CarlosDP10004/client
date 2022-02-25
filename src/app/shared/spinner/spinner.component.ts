119b5bacf37f892f8cd570a356f2e273ee463dc0
alicemenjivar@hotmail.es <alicemenjivar@hotmail.es>
2022-02-25 17:54:58 -0600

    se sube funcionalida de spinner

---
+++ src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from '../../core/http/spinner.service';
+
+@Component({
+  selector: 'app-spinner',
+  template: '<div class="overlay" *ngIf="isLoading$ | async"> <div class="lds-hourglass"> </div></div>',
+  styleUrls: ['./spinner.component.scss']
+})
+export class SpinnerComponent {
+
+  isLoading$ = this.spinnerSvc.isLoading$;
+
+  constructor(private spinnerSvc: SpinnerService) { }
+
+}

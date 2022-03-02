863bf1ecbedb960b3f3be44700adbd966c8cdb56
cdiaztester <carlos.diaz.dp10004@gmail.com>
2022-03-02 12:09:12 -0600

    Se sube fix para manejo de unauthorized y not found

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -1,24 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AuthService } from '../http/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(
-    private userService: AuthService, 
-    private router: Router
-    ){}    
+    constructor(
+      private userService: AuthService, 
+      private router: Router
+      ){}    
 
-  canActivate(): Observable<boolean>{
-
-    var isAuthenticated = this.userService.isLogged;
-        if (!isAuthenticated) {
+    canActivate(): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree 
+    {
+      return this.userService.isLogged.pipe(
+        map( user =>{
+          if(!user){
             this.router.navigate(['/login']);
-        }
-        return isAuthenticated;
-  }
-  
+            return false;
+          }
+          return true;
+        })
+      );
+    }
 }

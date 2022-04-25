369492dfa09d8f2be545d6236ceb5ee228bbedd1
Martin <martin_afii@hotmail.com>
2022-04-24 22:02:48 -0600

    Se arreglaron fixed en los modales de roles, en formularios de activo y el buscador de activo fijo

--- src/app/pipes/activo.pipe.ts
+++ src/app/pipes/activo.pipe.ts
@@ -5,18 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ActivoPipe implements PipeTransform {
 
-  transform(value: any, args?: any) {    
+  transform(value: any, args?: any): any {    
     if (!value)return null;
     if (!args)return value;    
     const resultPosts = [];   
     for(const asset of value){
-      if(asset.Descripcion.toLowerCase().includes(args.toLowerCase()) 
-      || asset.Marca.toLowerCase().includes(args.toLowerCase())
+      if(asset.CodigoAF.toLowerCase().includes(args.toLowerCase())
+      || asset.Descripcion.toLowerCase().includes(args.toLowerCase()) 
+      || asset.NombreMarca.toString().toLowerCase().includes(args.toLowerCase())
       || asset.Modelo.toLowerCase().includes(args.toLowerCase())
-      || asset.ValorCompra.toLowerCase().includes(args.toLowerCase())
-      || asset.CodigoAF.toLowerCase().includes(args.toLowerCase())
+      || asset.ValorCompra.toLowerCase().includes(args.toLowerCase())     
       || asset.FechaCompra.toString().toLowerCase().includes(args.toLowerCase())
-      || asset.NombreEstado.toLowerCase().includes(args.toLowerCase()) )
+      || asset.NombreEstado.toLowerCase().includes(args.toLowerCase())
+       )
       {
         resultPosts.push(asset);      };
       };

8de2b4f3d2b868e952631b0ea3ed2b7c15d39ada
cdiaztester <carlos.diaz.dp10004@gmail.com>
2022-07-12 20:07:38 -0600

    Se suben cambios por nuevos filtros en reportes

--- src/app/pages/reports/depreciation/depreciation.component.ts
+++ src/app/pages/reports/depreciation/depreciation.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from 'src/app/core/http/account.service';
+import { ErrorService } from 'src/app/core/http/error.service';
 import { ReportService } from 'src/app/core/http/report.service';
 import { SettingsService } from 'src/app/core/http/settings.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-depreciation',
@@ -19,20 +22,35 @@ export class DepreciationComponent implements OnInit {
   hasta: any;
   year: number;
   
+  accounts: any[] = [];
 
   constructor(
     private formBuilder:FormBuilder,
     private reportService: ReportService,
+    private accountService: AccountService,
+    private errorService: ErrorService,
     private settingService: SettingsService
   ) { }
 
   ngOnInit(): void {
     this.filters = this.formBuilder.group({
       Desde:['',[Validators.required]],
-      Hasta:['',[Validators.required]]
+      Hasta:['',[Validators.required]],
+      IdCuenta: new FormControl(this.formBuilder.array([]))
     });
     this.settingService.getTitle().subscribe(data => {
       this.Titulo = data['ValorCadena'];
+    });
+    this.accountService.getAccountList().subscribe(data => {
+      Object.assign(this.accounts, data);
+    }, error => { 
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: this.errorService.getErrorMessage(error.error),
+        confirmButtonColor: '#c9a892',
+        confirmButtonText: 'Aceptar'
+      }) 
     });
     let date: Date = new Date();
     this.year = date.getFullYear();
@@ -42,6 +60,7 @@ export class DepreciationComponent implements OnInit {
     let filter = {
       'Desde': this.filters.get('Desde').value,      
       'Hasta': this.filters.get('Hasta').value,
+      'IdCuenta': this.filters.get('IdCuenta').value,
     }
     this.setRange();    
     this.reportService.getDepreciation(filter).subscribe(data => {
